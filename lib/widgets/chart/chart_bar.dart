a72ca8f6641b70682605342fdc11c115313c65a7
Syed-Abdullah-G <syedabdullahcslab@gmail.com>
2024-04-23 18:08:07 +0530

    updates

--- lib/widgets/chart/chart_bar.dart
+++ lib/widgets/chart/chart_bar.dart
@@ -1,24 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:simple_app/models/expense.dart';
+import 'package:simple_app/widgets/chart/chart.dart';
+import 'package:simple_app/widgets/expenses.dart';
 
 class ChartBar extends StatelessWidget {
   const ChartBar({
     super.key,
     required this.fill,
     required this.amount,
+    required this.registeredExpenses,
   });
 
   final String amount;
   final double fill;
+  final List<Expense> registeredExpenses;
 
   @override
   Widget build(BuildContext context) {
+    double calculateTotalAmount() {
+      double totalAmount = 0.0;
+      for (final expense in registeredExpenses) {
+        totalAmount += expense.amount;
+      }
+      return totalAmount;
+    }
+
+    final amountinInt = double.parse(amount);
+    final showcaseAmountforPercentage = (amountinInt / calculateTotalAmount());
+    final showcaseAmountAfterConversion = showcaseAmountforPercentage * 100;
+    final String showCasePercentageString =
+        showcaseAmountAfterConversion.toStringAsFixed(2);
+
     final isDarkMode =
         MediaQuery.of(context).platformBrightness == Brightness.dark;
     return Expanded(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 4),
         child: Stack(children: [
-          Container(width: double.infinity,
+          Container(
+            width: double.infinity,
             child: FractionallySizedBox(
               heightFactor: fill,
               child: DecoratedBox(
@@ -33,7 +53,16 @@ class ChartBar extends StatelessWidget {
               ),
             ),
           ),
-          Positioned.fill(child: Center(child: Text(amount,style: TextStyle(fontSize: 17,color: Colors.black,fontWeight: FontWeight.w500),),))
+          Positioned.fill(
+              child: Center(
+            child: Text(
+              amount,
+              style: TextStyle(
+                  fontSize: 17,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w500),
+            ),
+          ))
         ]),
       ),
     );
